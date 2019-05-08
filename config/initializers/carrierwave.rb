cd19993a968a5908a01fbc04632c27915180244d
安松海里 <kairiyasumatsu@yasumatsukairinoMacBook-Pro.local>
2019-05-08 22:55:49 +0900

    AWS修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,11 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_KEY"],
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],#開発環境においては使わない設定となっているので、今は必要ないです
-    region: 'ap-northeast-1',
-    host:                  's3.amazonaws.com/geekphoto2',             # optional, defaults to nil
-    endpoint:              'https://s3.ap-northeast-1.amazonaws.com' # optional, defaults to nil
+    region: 'us-east-1',
+    host:                  's3.amazonaws.com/geekphoto',             # optional, defaults to nil
+    endpoint:              'https://s3.us-east-1.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'geekphoto2'                # required
+  config.fog_directory  = 'geekphoto'                # required
   config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
