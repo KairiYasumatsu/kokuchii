19861ec099b006dce5d7e0a1f8c00768fdc481cd
安松海里 <kairiyasumatsu@yasumatsukairinoMacBook-Pro.local>
2019-05-08 23:54:05 +0900

    AWS修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,12 +24,13 @@ class PostsController < ApplicationController
 
     prepare_meta_tags(og:{
       image: @post.image.to_s,
-      title: @post.title
+
       })
     prepare_meta_tags(title: @post.title)
     prepare_meta_tags(twitter:{
       title: @post.title,
-      image: @post.image.to_s
+      image: @post.image.to_s,
+      site_name: @post.title
       })
   end
 
