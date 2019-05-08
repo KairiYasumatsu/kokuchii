ad69e816b213b345d44073ef93a723466017a677
安松海里 <kairiyasumatsu@yasumatsukairinoMacBook-Pro.local>
2019-05-08 16:24:19 +0900

    点をを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_KEY"],
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],, #開発環境においては使わない設定となっているので、今は必要ないです
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],#開発環境においては使わない設定となっているので、今は必要ないです
     region: 'ap-northeast-1',
     host:                  's3.amazonaws.com/geek-rails-photo-sampler',             # optional, defaults to nil
     endpoint:              'https://s3.ap-northeast-1.amazonaws.com' # optional, defaults to nil
