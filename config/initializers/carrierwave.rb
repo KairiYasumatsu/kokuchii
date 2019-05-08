e708f3e1d4fce567e06b604b991accb2bfd3e638
安松海里 <kairiyasumatsu@yasumatsukairinoMacBook-Pro.local>
2019-05-08 16:15:04 +0900

    APIキーを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws' # required
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'KIAIILL4XN5KLL5QXTQ',
-    aws_secret_access_key: 'リリースする時に聞いてね！', #開発環境においては使わない設定となっているので、今は必要ないです
+    aws_access_key_id: ENV["AWS_KEY"],
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],, #開発環境においては使わない設定となっているので、今は必要ないです
     region: 'ap-northeast-1',
     host:                  's3.amazonaws.com/geek-rails-photo-sampler',             # optional, defaults to nil
     endpoint:              'https://s3.ap-northeast-1.amazonaws.com' # optional, defaults to nil
